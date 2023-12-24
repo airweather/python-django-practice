115bbe8b590eacb16900adc03f1fcaf0eaf722b2
Jun <wnsghd1@naver.com>
2023-12-24 19:09:55 +0900

    routing

--- urls.py
+++ urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+# http://127.0.0.1/
+# http://127.0.0.1/app/
+
+# http://127.0.0.1/create/
+# http://127.0.0.1/read/1
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
 ]
